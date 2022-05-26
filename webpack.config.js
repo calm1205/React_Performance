a988cf361a9491d97a8a9bc21d511e3f1e2b9e04
calm1205 <naginani072@gmail.com>
2022-05-27 08:11:19 +0900

    mv index.html

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@
  * core nodejs moduleの1つ。
  * デフォルトでnodejsが提供しているmodule
  */
-const path = require("path");
+const path = require('path');
 /**
  * このpluginでcontenthashが書き込まれたhtmlファイルの出力が可能
  * https://github.com/jantimon/html-webpack-plugin#options
  */
-const HtmlWebpackPulgin = require("html-webpack-plugin");
+const HtmlWebpackPulgin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main.tsx",
+  mode: 'development',
+  entry: './src/main.tsx',
   plugins: [
     new HtmlWebpackPulgin({
-      title: "React Sandbox",
-      favicon: path.resolve(__dirname, "src", "public", "favicon.ico"),
-      template: path.resolve(__dirname, "src", "index.html"),
+      title: 'React Sandbox',
+      favicon: path.resolve(__dirname, 'src', 'public', 'favicon.ico'),
+      template: path.resolve(__dirname, 'src', 'public', 'index.html'),
     }),
   ],
   output: {
@@ -25,13 +25,13 @@ module.exports = {
      * bundle後のファイル名が必ず同じファイル名だとコードの差分があってもcacheされたbundleファイルをクライアントが読み込んでしまう。
      * フィンガープリントがあることでコードの差分が出るとファイル名も変わるのでコードの差分がある場合はクライアント側でもcacheを破棄して新しいbundleファイルを読み込める。
      */
-    filename: "bundle.[contenthash].js",
+    filename: 'bundle.[contenthash].js',
     /**
      * __dirnameはnodeの変数で絶対パスを返す。
      * path.resolveを活用するのはパスの区切りが/でない場合もあるため。
      * path.resolveを活用すればパスの区切りを環境によって使い分けて結合してくれる
      */
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
 
     /**
      * pathで指定したコンパイル後のファイルを全削除してからbundleファイルを配置
@@ -41,7 +41,7 @@ module.exports = {
   /**
    * bundleファイルのsource-mapをdevtoolで確認できる。
    */
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   /**
    * https://webpack.js.org/guides/build-performance/#typescript-loader
    */
@@ -49,7 +49,7 @@ module.exports = {
     rules: [
       {
         test: /\.tsx$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
@@ -58,9 +58,9 @@ module.exports = {
    * tsファイルを直接bundleする際にimportの拡張子を補完する。
    */
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
     alias: {
-      "~": path.resolve(__dirname, "src"),
+      '~': path.resolve(__dirname, 'src'),
     },
   },
 };
