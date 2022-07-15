2074b26fd47098f9e5340246daae363509597049
calm1205 <naginani072@gmail.com>
2022-07-15 13:57:09 +0900

    feat(src/Main):

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from '~/components/pages/index';
 import { Memo } from '~/memo/Index';
-import { BeforeYouMemo } from './beforeYouMemo/Index';
-import { Router } from './router';
-import { HighCost } from './components/HighCost';
-import MeasureRenderingTime from './components/MeasureRenderingTime';
-import { NoNeedEffect } from './noNeedEffect/Index';
 
 const rootDOM = document.getElementById('root');
 
@@ -14,18 +10,8 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Router />} />
+        <Route path="/" element={<App />} />
         <Route path="/memo" element={<Memo />} />
-        <Route path="/beforeYouMemo" element={<BeforeYouMemo />} />
-        <Route path="/noNeedEffect" element={<NoNeedEffect />} />
-        <Route
-          path="/highCost"
-          element={
-            <MeasureRenderingTime label="high cost">
-              <HighCost />
-            </MeasureRenderingTime>
-          }
-        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
